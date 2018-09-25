a1d64e67aab35ad41cd5c1892c0ec13232d19612
nilesh.devdas@gmail.com <nilesh.devdas@gmail.com>
2018-09-25 13:09:43 +0530

    CPP Gradle Demo

---
+++ src/main/cpp/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>  
+#include "greeting.hpp"
+int main(int argc, char** argv) {
+    std::cout << greeting << std::endl;
+    std::cout << "This is cool" << std::endl;
+    return 0;
+}
